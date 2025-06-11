7deb18b8ce0dea2a9d5ff92167b706dd73ea8a54
Xelor-DEV <140673613+Xelor-DEV@users.noreply.github.com>
2025-06-11 12:04:04 -0500

    Action nodes elimitaed for new structure

---
+++ Assets/ProjectAssets/Scripts/FuzzyLogic/FuzzyLogic.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class FuzzyFunction
+{
+    [SerializeField] private string name;
+    [SerializeField] private AnimationCurve functionCurve;
+    [SerializeField] private float y;
+    [SerializeField] private float singletonValue;
+
+    public AnimationCurve FunctionCurve
+    {
+        get
+        {
+            return functionCurve;
+        }
+        set
+        {
+            functionCurve = value;
+        }
+    }
+
+    public float Y
+    {
+        get
+        {
+            return y;
+        }
+        set
+        {
+            y = value;
+        }
+    }
+
+    public float SingletonValue
+    {
+        get
+        {
+            return singletonValue;
+        }
+        set
+        {
+            singletonValue = value;
+        }
+    }
+
+    public float Evaluate(float x)
+    {
+        y = 0;
+        if (x >= functionCurve.keys[0].time)
+        {
+            y += Mathf.Clamp01(functionCurve.Evaluate(x));
+        }
+        return y;
+    }
+}
+
+[Serializable]
+public class CalculateFuzzy
+{
+    [SerializeField] private FuzzyFunction[] membershipFunctions;
+
+    public float CalculateFuzzyValue(float distance)
+    {
+        float sumW = 0;
+        float multW = 0;
+
+        for (int i = 0; i < membershipFunctions.Length; ++i)
+        {
+            float y = membershipFunctions[i].Evaluate(distance);
+            sumW += y;
+            multW += y * membershipFunctions[i].SingletonValue;
+        }
+
+        if (sumW != 0)
+        {
+            return multW / sumW;
+        }
+        else
+        {
+            return multW;
+        }
+    }
+}
